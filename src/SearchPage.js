41802326eaea140da6f7e4f68537053baac35c46
Kamsi Kodi <kamsikodi@gmail.com>
2020-08-08 10:22:42 +0100

    Finished Search Page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './SearchPage.css';
 import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined';
 import ChannelRow from './ChannelRow';
+import VideoRow from './VideoRow';
 
 function SearchPage() {
     return (
@@ -20,6 +21,24 @@ function SearchPage() {
                 description="This is a fully comprehensive python course with exercises, notes, solutions, and projects. We will cover all fundamentals of python programming and give you a solid understanding of things like "
             />
             <hr />
+            <VideoRow 
+                views="1.4M"
+                subs="659K"
+                description="Do you want a free online course"
+                timestamp="59 seconds ago"
+                channel="Clever Programmer"
+                title="Lets Build an application"
+                image="https://cdn.geekwire.com/wp-content/uploads/2019/07/bigstock-Real-Python-Code-Developing-Sc-217216555-630x421.jpg"
+            />
+            <VideoRow 
+                views="1.4M"
+                subs="659K"
+                description="Do you want a free online course"
+                timestamp="59 seconds ago"
+                channel="Clever Programmer"
+                title="Lets Build an application"
+                image="https://cdn.geekwire.com/wp-content/uploads/2019/07/bigstock-Real-Python-Code-Developing-Sc-217216555-630x421.jpg"
+            />
         </div>
     )
 }
