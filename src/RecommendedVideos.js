a12ba71c4efd8cb4b80089b00cde198c5fa0af4c
Kamsi Kodi <kamsikodi@gmail.com>
2020-08-08 08:50:23 +0100

    Added Recommended Video Feature

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,10 +1,84 @@
 import React from 'react'
 import './RecommendedVideos.css';
-
+import VideoCard from './VideoCard';
 function RecommendedVideos() {
     return (
         <div className="recommendedVideos">
-            <h1>RecommendedVideos</h1>
+            <h2>Recommended Videos</h2>
+            <div className="recommended__videos">
+                <VideoCard
+                    title="Become a web designer in three days | 2019/2020"
+                    views="2.3M views"
+                    timestamp="3 days ago"
+                    channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6zes53m4a_2VLTcmTn_bHk8NO5SkuWfcQbg&usqp=CAU"
+                    channel="Kamsi Kodi"
+                    image="https://cdn-media-1.freecodecamp.org/images/0*ngXgBNNdx6iiWP8q.png"
+                />
+                <VideoCard
+                    title="Better programming in three days | 2019/2020"
+                    views="2.3M views"
+                    timestamp="3 days ago"
+                    channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6zes53m4a_2VLTcmTn_bHk8NO5SkuWfcQbg&usqp=CAU"
+                    channel="Kamsi Kodi"
+                    image="https://hackr.io/blog/what-is-programming/thumbnail/large"
+                />
+                <VideoCard
+                    title="Everything you need to know | 2019/2020"
+                    views="2.3M views"
+                    timestamp="3 days ago"
+                    channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6zes53m4a_2VLTcmTn_bHk8NO5SkuWfcQbg&usqp=CAU"
+                    channel="Kamsi Kodi"
+                    image="https://cdn.geekwire.com/wp-content/uploads/2019/07/bigstock-Real-Python-Code-Developing-Sc-217216555-630x421.jpg"
+                />
+                <VideoCard
+                    title="Become a web designer in three days | 2019/2020"
+                    views="2.3M views"
+                    timestamp="3 days ago"
+                    channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6zes53m4a_2VLTcmTn_bHk8NO5SkuWfcQbg&usqp=CAU"
+                    channel="Kamsi Kodi"
+                    image="https://cdn-media-1.freecodecamp.org/images/0*ngXgBNNdx6iiWP8q.png"
+                />
+                <VideoCard
+                    title="Better programming in three days | 2019/2020"
+                    views="2.3M views"
+                    timestamp="3 days ago"
+                    channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6zes53m4a_2VLTcmTn_bHk8NO5SkuWfcQbg&usqp=CAU"
+                    channel="Kamsi Kodi"
+                    image="https://hackr.io/blog/what-is-programming/thumbnail/large"
+                />
+                <VideoCard
+                    title="Everything you need to know | 2019/2020"
+                    views="2.3M views"
+                    timestamp="3 days ago"
+                    channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6zes53m4a_2VLTcmTn_bHk8NO5SkuWfcQbg&usqp=CAU"
+                    channel="Kamsi Kodi"
+                    image="https://cdn.geekwire.com/wp-content/uploads/2019/07/bigstock-Real-Python-Code-Developing-Sc-217216555-630x421.jpg"
+                />
+                <VideoCard
+                    title="Become a web designer in three days | 2019/2020"
+                    views="2.3M views"
+                    timestamp="3 days ago"
+                    channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6zes53m4a_2VLTcmTn_bHk8NO5SkuWfcQbg&usqp=CAU"
+                    channel="Kamsi Kodi"
+                    image="https://cdn-media-1.freecodecamp.org/images/0*ngXgBNNdx6iiWP8q.png"
+                />
+                <VideoCard
+                    title="Better programming in three days | 2019/2020"
+                    views="2.3M views"
+                    timestamp="3 days ago"
+                    channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6zes53m4a_2VLTcmTn_bHk8NO5SkuWfcQbg&usqp=CAU"
+                    channel="Kamsi Kodi"
+                    image="https://hackr.io/blog/what-is-programming/thumbnail/large"
+                />
+                <VideoCard
+                    title="Everything you need to know | 2019/2020"
+                    views="2.3M views"
+                    timestamp="3 days ago"
+                    channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6zes53m4a_2VLTcmTn_bHk8NO5SkuWfcQbg&usqp=CAU"
+                    channel="Kamsi Kodi"
+                    image="https://cdn.geekwire.com/wp-content/uploads/2019/07/bigstock-Real-Python-Code-Developing-Sc-217216555-630x421.jpg"
+                />
+            </div>
         </div>
     )
 }
